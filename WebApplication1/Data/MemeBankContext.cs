0a939a780015258bf6ec7aee7de292ca53673bad
In Ha Ryu <iryu815@aucklanduni.ac.nz>
2018-11-17 14:57:21 +1300

    example deployment

---
+++ WebApplication1/Data/MemeBankContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models
+{
+    public class MemeBankContext : DbContext
+    {
+        public MemeBankContext (DbContextOptions<MemeBankContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplication1.Models.MemeItem> MemeItem { get; set; }
+    }
+}
